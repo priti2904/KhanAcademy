38d958df2e4c7ec32ebe847a7df1eb53cf432f2c
priti2904 <pritikale829@gmail.com>
2022-11-11 11:26:55 +0530

    Added pom package and class

--- src/main/java/pom/KhanAcademyLoginpage.java
+++ src/main/java/pom/KhanAcademyLoginpage.java
@@ -1,5 +1,13 @@
 package pom;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class KhanAcademyLoginpage {
 
+	@FindBy() private WebElement xyz;
+ 
+	@FindBy() private WebElement ABC;
+	
+	@FindBy() private WebElement PYQ;
 }
